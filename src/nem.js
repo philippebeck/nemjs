ca281d72987799f2ea6587ab0486e6e0e4081fc9
Philippe Beck <philippe@philippebeck.net>
2023-01-12 23:06:48 +0400

    Create createImage() function

--- src/nem.js
+++ src/nem.js
@@ -106,7 +106,29 @@ exports.checkPass = (pass, res) => {
 }
 
 /**
- * CREATE CONTACT MAILER
+ * CREATE IMAGE
+ * @param {string} inputImg 
+ * @param {string} outputImg 
+ * @returns
+ */
+exports.createImage = (inputImg, outputImg) => {
+  const sharp = require('sharp');
+
+  sharp(process.env.IMG_URL + inputImg)
+    .resize(
+      parseInt(process.env.IMG_WIDTH, 10), 
+      parseInt(process.env.IMG_HEIGHT, 10),
+      { 
+        fit: process.env.IMG_FIT,
+        position: process.env.IMG_POSITION 
+      }
+    )
+    .toFormat(process.env.IMG_EXT)
+    .toFile(process.env.IMG_URL + outputImg);
+}
+
+/**
+ * CREATE MAILER
  * @returns 
  */
 exports.createMailer = () => {
