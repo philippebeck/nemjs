1d796897c6acaa33ee4b4f27f44e31eefda3918a
Philippe Beck <philippe@philippebeck.net>
2023-02-04 03:10:52 +0400

    Create getImgName()

--- src/image.js
+++ src/image.js
@@ -41,3 +41,16 @@ exports.createThumbnail = (inputImg, outputImg) => {
     .toFormat(process.env.THUMB_EXT)
     .toFile(process.env.THUMB_URL + outputImg);
 }
+
+/**
+ * GET IMAGE NAME
+ * @param {string} name 
+ */
+exports.getImgName = (name) => {
+  const accents = require("remove-accents");
+
+  return accents
+    .remove(name)
+    .replace(/ /g, "-")
+    .toLowerCase() + "-" + Date.now() + "." + process.env.IMG_EXT;
+}
