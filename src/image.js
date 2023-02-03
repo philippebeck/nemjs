395abdb3e4c499509e2cef6d425d83fe866eb48e
Philippe Beck <philippe@philippebeck.net>
2023-02-04 03:10:40 +0400

    Create createThumbnail()

--- src/image.js
+++ src/image.js
@@ -19,3 +19,25 @@ exports.createImage = (inputImg, outputImg) => {
     .toFormat(process.env.IMG_EXT)
     .toFile(process.env.IMG_URL + outputImg);
 }
+
+/**
+ * CREATE THUMBNAIL
+ * @param {string} inputImg 
+ * @param {string} outputImg 
+ * @returns
+ */
+exports.createThumbnail = (inputImg, outputImg) => {
+  const sharp = require('sharp');
+
+  sharp(process.env.IMG_URL + inputImg)
+    .resize(
+      parseInt(process.env.THUMB_WIDTH, 10), 
+      parseInt(process.env.THUMB_HEIGHT, 10),
+      { 
+        fit: process.env.THUMB_FIT,
+        position: process.env.IMG_POSITION 
+      }
+    )
+    .toFormat(process.env.THUMB_EXT)
+    .toFile(process.env.THUMB_URL + outputImg);
+}
