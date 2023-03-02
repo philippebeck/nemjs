712649029b36546ed3f8c7c4c75fc31222dbb352
Philippe Beck <philippe@philippebeck.net>
2023-03-02 11:25:09 +0400

    Rename string file to checker & move generatePass() out

--- src/checker.js
+++ src/checker.js
@@ -81,20 +81,3 @@ exports.checkUrl = (url) => {
 
   return false;
 }
-
-/**
- * GENERATE PASSWORD
- * @returns 
- */
-exports.generatePass = () => {
-  const generator = require("generate-password");
-
-  let pass = generator.generate({
-    length: process.env.GENERATE_LENGTH,
-    numbers: process.env.GENERATE_NUMBERS,
-    symbols: process.env.GENERATE_SYMBOLS,
-    strict: process.env.GENERATE_STRICT
-  });
-
-  return pass;
-}
