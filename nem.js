1e78d80f2764edba0e1a6cc6462f56a6982af8f9
Philippe Beck <philippe@philippebeck.net>
2023-03-05 15:35:22 +0400

    Release 0.10.0

--- nem.js
+++ nem.js
@@ -1,4 +1,4 @@
-/*! nemjs v0.9.0 | https://www.npmjs.com/package/nemjs | Apache-2.0 License */
+/*! nemjs v0.10.0 | https://www.npmjs.com/package/nemjs | Apache-2.0 License */
 
 "use strict";
 
@@ -49,14 +49,16 @@ exports.checkEmail = (email) => {
 }
 
 /**
- * CHECK NAME
- * @param {string} name 
+ * CHECK NUMBER
+ * @param {number} number
+ * @param {number} min
+ * @param {number} max
  * @returns 
  */
-exports.checkName = (name) => {
-  if (name.length >= process.env.NAME_MIN && 
-    name.length <= process.env.NAME_MAX) {
+exports.checkNumber = (number, min = process.env.NUM_MIN, max = process.env.NUM_MAX) => {
+  number = Number(number);
 
+  if (number >= min && number <= max) {
     return true;
   }
 
@@ -89,27 +91,15 @@ exports.checkPass = (pass) => {
 
 /**
  * CHECK STRING
- * @param {string} string 
+ * @param {string} string
+ * @param {number} min
+ * @param {number} max
  * @returns 
  */
-exports.checkString = (string) => {
-  if (typeof string === "string" && string !== "") {
+exports.checkString = (string, min = process.env.STRING_MIN, max = process.env.STRING_MAX) => {
+  string = String(string);
 
-    return true;
-  }
-
-  return false;
-}
-
-/**
- * CHECK TEXT
- * @param {string} text 
- * @returns 
- */
-exports.checkText = (text) => {
-  if (text.length >= process.env.TEXT_MIN && 
-    text.length <= process.env.TEXT_MAX) {
-
+  if (string.length >= min && string.length <= max) {
     return true;
   }
 
