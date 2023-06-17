837e9941cef9d9428a648e06fa45799e81972970
Philippe Beck <philippe@philippebeck.net>
2023-06-17 12:30:30 +0400

    Fix typo to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
         banner: "/*! <%= pkg.name %> v<%= pkg.version %> | <%= pkg.homepage %> | <%= pkg.license %> */\n\n", 
         footer: "\n/*! Author: <%= pkg.author.name %> <<%= pkg.author.email %>> | Updated: <%= grunt.template.today('dS mmm yyyy') %> */"},
       js: {
-        src: ["src/init.js", "src/checker.js", "src/getter.js", "src/setter.js"],
+        src: ["src/init.js", "src/checkers.js", "src/getters.js", "src/setters.js"],
         dest: "nem.js"}
     }
   });
